ec36b7c8e5fbf23b577735df4acbccf132536990
G.Charkiewicz <charkiewicz.grzegorz@gmail.com>
2020-08-02 13:31:33 +0200

    Retrieving an item based on the value of the Class attribute

---
+++ Javascript-and-jquery-by-jon-duckett/c05/js/get-elements-by-class-name.js
@@ -0,0 +1,8 @@
+var elements = document.getElementsByClassName('hot');
+
+if(elements.length >2){
+    //var firstItem = elements.item(0);
+    //firstItem.className = 'cool';
+    var el = elements[2];
+    el.className = 'cool';
+}
