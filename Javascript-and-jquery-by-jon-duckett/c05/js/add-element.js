b7d5663061a1d2b96d258e3fc8f3a194c9911b2c
G.Charkiewicz <charkiewicz.grzegorz@gmail.com>
2020-08-11 20:44:49 +0200

    Add an item in the house model tree.

---
+++ Javascript-and-jquery-by-jon-duckett/c05/js/add-element.js
@@ -0,0 +1,9 @@
+var newEl = document.createElement('li');
+
+var newText = document.createTextNode('komosa ryżowa');
+
+newEl.appendChild(newText);
+
+var position = document.getElementsByTagName('ul')[0];
+
+position.appendChild(newEl);
