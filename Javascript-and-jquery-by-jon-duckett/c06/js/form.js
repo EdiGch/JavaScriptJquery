3e0f6c43bfb6dc94aa10b6f2665a422f65d5d59f
G.Charkiewicz <charkiewicz.grzegorz@gmail.com>
2020-08-25 15:27:02 +0200

    Use of web form events.

---
+++ Javascript-and-jquery-by-jon-duckett/c06/js/form.js
@@ -0,0 +1,33 @@
+var elForm, elSelectPackage, elPackageHint, elTerms, elTermsHint; // Declare variables
+elForm          = document.getElementById('formSignup');          // Store elements
+elSelectPackage = document.getElementById('package');
+elPackageHint   = document.getElementById('packageHint');
+elTerms         = document.getElementById('terms');
+elTermsHint     = document.getElementById('termsHint');
+
+console.log(elForm);
+console.log(elSelectPackage);
+console.log(elPackageHint);
+console.log(elTerms);
+console.log(elTermsHint);
+
+function packageHint() {                                 // Declare function
+var pack = this.options[this.selectedIndex].value;
+console.log(pack);
+  if (pack === 'monthly') {                              // If monthly package
+    elPackageHint.innerHTML = 'Save $10 if you pay for 1 year!';//Show this msg
+  } else {                                               // Otherwise
+    elPackageHint.innerHTML = 'Wise choice!';            // Show this message
+  }
+}
+
+function checkTerms(event) {                             // Declare function
+  if (!elTerms.checked) {                                // If checkbox ticked
+    elTermsHint.innerHTML = 'You must agree to the terms.'; // Show message
+    event.preventDefault();                              // Don't submit form
+  }
+}
+
+//Create event listeners: submit calls checkTerms(), change calls packageHint()
+elForm.addEventListener('submit', checkTerms, false);
+elSelectPackage.addEventListener('change', packageHint, false);
